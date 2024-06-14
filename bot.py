0d0f47270c999c42941b3922983dcf20b3175176
zappurenfuro <zarenfro23@gmail.com>
2024-06-15 02:00:16 +0700

    v1.0

--- bot.py
+++ bot.py
@@ -1,35 +1,19 @@
 import os
 import nextcord
 from nextcord.ext import commands
-from flask import Flask
-import asyncio
+from keep_alive import keep_alive
+import config
 
-# Initialize Flask app
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Albion Guild Assistance bot is running."
-
-# Initialize the bot
 intents = nextcord.Intents.default()
-intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
-# Load your cogs here
-bot.load_extension("cogs.balance")
+# Load cogs
+initial_extensions = ['cogs.balance']
 
-async def run_bot():
-    await bot.start(os.environ.get('BOT_TOKEN'))
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
 
-async def run_flask():
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-
-async def main():
-    bot_task = asyncio.create_task(run_bot())
-    flask_task = asyncio.create_task(run_flask())
-    await asyncio.gather(bot_task, flask_task)
-
-if __name__ == "__main__":
-    asyncio.run(main())
+    keep_alive()
+    bot.run(config.BOT_TOKEN)
+    
