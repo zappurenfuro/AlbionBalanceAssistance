ae3f4e5e2254acceb7f29e681f3600350e5470bb
zappurenfuro <zarenfro23@gmail.com>
2024-06-15 01:40:15 +0700

    v1.0

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import nextcord
 from nextcord.ext import commands
 from flask import Flask
 from threading import Thread
-from utils.helpers import add_balance, get_balance, add_proof, get_proofs, get_total_balance, update_treasury, get_treasury_balance
+import asyncio
 import config
 
 # Initialize Flask app
@@ -21,24 +21,17 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 # Add your cogs here
 bot.load_extension("cogs.balance")
 
-# Function to run the bot
-def run_bot():
-    bot.run(config.BOT_TOKEN)
+async def run_bot():
+    await bot.start(config.BOT_TOKEN)
 
-# Function to run the Flask server
-def run_flask():
+async def run_flask():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
 
+async def main():
+    bot_task = asyncio.create_task(run_bot())
+    flask_task = asyncio.create_task(run_flask())
+    await asyncio.gather(bot_task, flask_task)
+
 if __name__ == "__main__":
-    # Create two threads to run both the Flask server and the Discord bot
-    flask_thread = Thread(target=run_flask)
-    bot_thread = Thread(target=run_bot)
-
-    # Start both threads
-    flask_thread.start()
-    bot_thread.start()
-
-    # Wait for both threads to complete
-    flask_thread.join()
-    bot_thread.join()
+    asyncio.run(main())
